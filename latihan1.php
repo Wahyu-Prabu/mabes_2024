ac43d248bdb4aec507544aad67f06733eb035504
Wahyu-Prabu <prabuwahyu@yahoo.co.id>
2024-07-01 12:01:56 +0700

    ini file pertama

---
+++ latihan1.php
@@ -0,0 +1,14 @@
+<?php
+
+
+echo"Hello World!";
+//perintah utk add atau tambah
+//1. git add . atau git add nama_folder/file
+//perintah untuk commit atau komentar
+//2. git commit -m "komnetarnya apa"
+//perintah untuk upload atau rush
+//3. git push -u origin master
+//push adalah perintah, orogon adalah nama remote
+//master adalah nama branch
+
+?>
